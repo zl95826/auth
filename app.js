8a5efc71c24e672f809be1ccde98889a31e2a0a0
zl95826 <zl95826@hotmail.com>
2024-01-18 17:33:23 -0800

    made mistakes, sendFile vs res.json()

--- app.js
+++ app.js
@@ -31,19 +31,19 @@ app.use(
 );
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  //   req.session.userData = {
-  //     userId: "123",
-  //     userName: "JohnDoe",
-  //     userType: "admin",
-  //   };
-  // comment out the userData, mySessions table would have no data
-  // If you have an empty session object or there's no new data added to the session during a request,
-  // the session store may not initiate a storage operation(=may not save anything) because there's no new information to keep.
-  // So, when checking your MongoDB collection for session data, it's helpful to perform actions in your
-  // application that involve adding or modifying data in the session.
-
-  // res.send("welcome to the session demo." + JSON.stringify(req.session));
   res.sendFile("index.html", { root: "public" });
+});
+app.get("/register", (req, res) => {
+  res.sendFile("register.html", { root: "public" });
+});
+app.get("/log", (req, res) => {
+  res.sendFile("log.html", { root: "public" });
+});
+app.get("/logs", (req, res) => {
+  res.json({ text: "Hello World" });
+});
+app.use((req, res, next) => {
+  res.status(404).send("404 errors");
 });
 app.listen(3000, () => {
   console.log("test session");
